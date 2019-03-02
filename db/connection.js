b2de5454546b147cbd3dbfb5fd22d79638fc63e3
shread1234 <shread1234@hotmail.co.uk>
2019-03-02 13:07:36 +0000

    Heroku Setup Completed

--- db/connection.js
+++ db/connection.js
@@ -4,3 +4,11 @@ const dbconfig = require('../knexfile');
 const connection = knex(dbconfig);
 
 module.exports = connection;
+
+const ENV = process.env.NODE_ENV || 'development';
+const config =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile');
+
+module.exports = require('knex')(config);
