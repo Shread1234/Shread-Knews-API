6eb8f27bec16ddcfd92db41632eaad353f156416
shread1234 <shread1234@hotmail.co.uk>
2019-03-08 14:39:42 +0000

    updated comment created at date

--- db/migrations/20190211134626_create_comments_table.js
+++ db/migrations/20190211134626_create_comments_table.js
@@ -1,4 +1,4 @@
-exports.up = function (knex, Promise) {
+exports.up = function(knex, Promise) {
   const now = new Date(Date.now());
   return knex.schema.createTable('comments', (comments) => {
     comments.increments('comment_id').primary();
@@ -12,11 +12,11 @@ exports.up = function (knex, Promise) {
       .notNullable()
       .onDelete('CASCADE');
     comments.integer('votes').defaultTo(0);
-    comments.string('created_at').defaultTo(now.toUTCString());
+    comments.string('created_at').defaultTo(knex.fn.now());
     comments.string('body', 10485760).notNullable();
   });
 };
 
-exports.down = function (knex, Promise) {
+exports.down = function(knex, Promise) {
   return knex.schema.dropTable('comments');
 };
